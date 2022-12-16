842c7bf7ccc49b4834728dd16ef613c610b6452a
harshita <jainharshita944@gmail.com>
2022-12-16 13:29:06 +0530

    my commit

--- routes/users.js
+++ routes/users.js
@@ -24,6 +24,13 @@ const users = [
     firstName: "Vishal",
     lastName: "Jain",
   },
+  {
+    id: 1,
+    username: "abc",
+    password: "test",
+    firstName: "a",
+    lastName: "User",
+  },
 ];
 
 /* GET users listing. */
